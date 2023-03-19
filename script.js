ac9971af8772ccc8e666f7a5e2e7f1878de8c10a
sergey <sergeyxyzz@yandex.ru>
2023-03-19 10:17:37 +0300

    небольшая модификация

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?')
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?')
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -32,13 +9,24 @@ const personalMovieDB = {
     privat: false
 }
 
-const lastFilm = prompt('Последний фильм, который вы посомтрели')
-const gradeFilm = prompt('На сколько его оцените?')
-const lastFilm2 = prompt('Последний фильм, который вы посомтрели')
-const gradeFilm2 = prompt('На сколько его оцените?')
+for (let i = 0; i < 2; i++) {
+    let lastFilm = prompt('Последний фильм, который вы посомтрели')
+    let gradeFilm = prompt('На сколько его оцените?')   
+    if (lastFilm == null || gradeFilm == null || lastFilm.length > 50 || gradeFilm.length > 50) {
+        i--
+        lastFilm = prompt('Последний фильм, который вы посомтрели')
+    }
+    personalMovieDB.movies[lastFilm] = gradeFilm
+}
 
-personalMovieDB.movies[lastFilm] = gradeFilm
-personalMovieDB.movies[lastFilm2] = gradeFilm2
+if (personalMovieDB.count < 10) {
+    console.log('просмотрено мало фильмов')
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('классический зритель')
+} else {
+    console.log('Вы киноман')
+}
+
 
 
 console.log(personalMovieDB)
